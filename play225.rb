649db893ef9bf2fd8bc875065a8de51f14366660
古崎義尊 <breezebox@breezebox.local>
2014-10-11 14:06:41 +0900

    change

--- play225.rb
+++ play225.rb
@@ -1,15 +1,33 @@
+
 require "formula"
 
 class Play225 < Formula
   homepage "http://www.playframework.org/"
   url "http://downloads.typesafe.com/play/2.2.5/play-2.2.5.zip"
-  sha1 "18d44b5bc6dea0188a448920e40b659dd23ffef3"
+  sha1 ""
 
-  conflicts_with "sox", :because => "both install `play` binaries"
+  # conflicts_with "sox", :because => "both install `play` binaries"
+
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+
     rm_rf Dir["**/*.bat"]
     libexec.install Dir["*"]
     bin.install_symlink libexec/"play"
   end
+
 end
+
+
